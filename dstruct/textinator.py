50f43c4f3f1efa25f2e40800c4e57eb1d1e8f448
nbusich <usich.n@northeastern.edu>
2024-11-24 14:08:27 -0500

    Made pdf parser
    This uses PDF plumber, I will try other ones to get better results

--- dstruct/textinator.py
+++ dstruct/textinator.py
@@ -6,12 +6,13 @@ Output:
 """
 import random as rnd
 from collections import defaultdict, Counter
-
+import pdfplumber
+import json
+import os
 
 class Textinator:
     def __init__(self):
         """ Constructor
-
         datakey --> (filelabel --> datavalue)
         """
         self.data = defaultdict(dict)
@@ -40,23 +41,52 @@ class Textinator:
             'wordcount': Counter("To be or not to be".split(" ")),
             'numwords': rnd.randrange(10, 50)
         }
-
         return results
 
-    def json_parser(filename):
+    def json_parser(self, filename):
         f = open(filename)
         raw = json.load(f)
         text = raw['text']
         words = text.split(" ")
         wc = Counter(words)
         num = len(words)
-
         return {'wordcount': wc, 'numwords': num}
 
-        def pdf_parser(filename):
-            f = open(filename)
+    def pdf_parser(self, filename):
+        base_name = os.path.splitext(os.path.basename(filename))[0]
+        output_name = f'data/converted_files/{base_name}.txt'
+        fulltext = ''
+        with pdfplumber.open(filename) as pdf:
+            for page in pdf.pages:
+                text = page.extract_text()
+                fulltext += text
+        with open(output_name, 'w') as file:
+            file.write(fulltext)
 
-        def load_stop_words(self, stopwords_file):
-            pass
+        words = fulltext.split(" ")
+        wc = Counter(words)
+        num = len(words)
+        return {'wordcount': wc, 'numwords': num}
+
+
+    def load_stop_words(self, stopwords_file):
+        pass
         
-    
+
+def main():
+    T = Textinator()
+    T.load_text('data/cig_data/independent_1.pdf', 'I1', parser=T.pdf_parser)
+    T.load_text('data/cig_data/independent_2.pdf', 'I1', parser=T.pdf_parser)
+    T.load_text('data/cig_data/independent_3.pdf', 'I1', parser=T.pdf_parser)
+    T.load_text('data/cig_data/independent_4.pdf', 'I1', parser=T.pdf_parser)
+    T.load_text('data/cig_data/independent_5.pdf', 'I1', parser=T.pdf_parser)
+    T.load_text('data/cig_data/independent_6.pdf', 'I1', parser=T.pdf_parser)
+    T.load_text('data/cig_data/industry_sponsored_1.pdf', 'S1', parser=T.pdf_parser)
+    T.load_text('data/cig_data/industry_sponsored_2.pdf', 'S2', parser=T.pdf_parser)
+    T.load_text('data/cig_data/industry_sponsored_3.pdf', 'S3', parser=T.pdf_parser)
+    T.load_text('data/cig_data/industry_sponsored_4.pdf', 'S4', parser=T.pdf_parser)
+    T.load_text('data/cig_data/industry_sponsored_5.pdf', 'S5', parser=T.pdf_parser)
+    T.load_text('data/cig_data/industry_sponsored_6.pdf', 'S6')
+
+if __name__ == '__main__':
+    main()
