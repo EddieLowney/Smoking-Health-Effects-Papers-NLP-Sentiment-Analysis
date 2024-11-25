25896558891c90e4e4223645c31666708efcf337
EddieLowney <162757214+EddieLowney@users.noreply.github.com>
2024-11-25 17:44:09 -0500

    All comments

--- dstruct/textinator.py
+++ dstruct/textinator.py
@@ -5,24 +5,24 @@ anything that we want to visualize in the visionator
 Output:
 """
 
-
+import random as rnd
 from collections import defaultdict, Counter
 from pdfminer.high_level import extract_text
 import json
 import os
 import re
-from myopenai import MyOpenAPI
+# from myopenai import MyOpenAPI
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 import dotenv
 import pandas as pd
 import vaderSentiment.vaderSentiment as vs
-from SANKEY import show_sankey
+from sankey import show_sankey
 
 
 dotenv.load_dotenv()
-api = MyOpenAPI()
+# api = MyOpenAPI()
 
-STOP_WORDS_FILENAME = 'data/stop_words.txt'
+STOP_WORDS_FILENAME = 'stop_words.txt'
 
 class Textinator:
     def __init__(self):
@@ -31,20 +31,6 @@ class Textinator:
         """
         self.data = defaultdict(dict)
         self.stop_list = list()
-
-    def GPT_key_sections(self, text, filename):
-        base_name = os.path.splitext(os.path.basename(filename))[0]
-        output_name = f'data/GPT_sectioned/{base_name}.txt'
-        prompt = """Find the sections of this text that really contribute to\
-        its meaning. Example: find the abstract, key defining sentences,\
-        discussion, and conclusion of a research paper. Only return exactly\
-        what the article says. TEXT: """
-        prompt += text
-        response_text = api.ask(prompt=prompt)
-        response_text = re.sub(r"\*\*.*?\*\*", '', response_text)
-        response_text = re.sub(r"-", '', response_text)
-        with open(output_name, 'w') as file:
-            file.write(response_text)
 
     def load_text(self, filename, label=None, parser=None):
         """ Register a document with the framework.
@@ -89,23 +75,12 @@ class Textinator:
         """ Parse a standard text file and produce
         extract data results in the form of a dictionary. """
 
-        base_name = os.path.splitext(os.path.basename(filename))[0]
-        output_name = f'data/converted_files/{base_name}.txt'
+        results = {
+            'wordcount': Counter("To be or not to be".split(" ")),
+            'numwords' : rnd.randrange(10, 50)
+        }
 
-        with open(filename, 'r') as file:
-            text = file.read()
-        self.GPT_key_sections(text, filename)
-
-        with open(output_name, 'w') as file:
-            file.write(text)
-
-        # Filters words
-        cleaned_words = self.filter_words(text.split(" "))
-        # Gets word counts in a Counter datatype
-        wc = Counter(cleaned_words)
-        num = len(cleaned_words)
-
-        return {'wordcount': wc, 'numwords': num}
+        return results
 
     def json_parser(self, filename):
         f = open(filename)
@@ -116,6 +91,27 @@ class Textinator:
         num = len(words)
         return {'wordcount': wc, 'numwords': num}
 
+    def GPT_key_sections(self, text, filename):
+        """Given a text, calls the GPT API and prompts it to locate and rewrite
+        the most important portions of the text to a new file."""
+        # Gets the filename of the text
+        base_name = os.path.splitext(os.path.basename(filename))[0]
+
+        # Generates the name of the file for the GPT output and the prompt
+        output_name = f'data/GPT_sectioned/{base_name}.txt'
+        prompt = """Find the sections of this text that really contribute to\
+        its meaning. Example: find the abstract, key defining sentences,\
+        discussion, and conclusion of a research paper. Only return exactly\
+        what the article says. TEXT: """
+        prompt += text
+
+        # Calls GPT API and formats output
+        response_text = api.ask(prompt=prompt)
+        response_text = re.sub(r"\*\*.*?\*\*", '', response_text)
+        response_text = re.sub(r"-", '', response_text)
+        # Writes output to the specified file
+        with open(output_name, 'w') as file:
+            file.write(response_text)
 
     def pdf_parser(self, filename):
         """Called to parse a PDF file. Extracts text. Uses a PDF library
@@ -126,7 +122,7 @@ class Textinator:
         output_name = f'data/converted_files/{base_name}.txt'
 
         text = extract_text(filename)
-        #Writes most important portions of text to separate files using GPT
+        # Writes most important portions of text to separate files using GPT
         self.GPT_key_sections(text, filename)
         with open(output_name, 'w') as file:
             file.write(text)
@@ -136,15 +132,17 @@ class Textinator:
         # Gets word counts in a Counter datatype
         wc = Counter(cleaned_words)
         num = len(cleaned_words)
-        
+
         return {'wordcount': wc, 'numwords': num}
 
     def load_stop_words(self, stopwords_file):
+        """Loads stopwords from a text file"""
         with open(stopwords_file) as infile:
             for i in infile:
                 self.stop_list.append(i.strip())
 
     def ASBA_scores(self, filename):
+        """"""
         base_name = os.path.splitext(os.path.basename(filename))[0]
         output_name = f'results/ASBA/{base_name}.csv'
         with open(filename, "r") as file:
@@ -175,76 +173,102 @@ class Textinator:
         return df
 
     def wordcount_sankey(self, word_list = None, k = 5):
-        """"""
+        """Given a list of words and a number of top most common words to be
+        found from the text stored as state variables within the class,
+        generates a sankey diagram using including either the words in the list
+        passed to the function, or, if no list was passed, the union of the
+        k most common words from each text in the class."""
+
+        # Creates two empty dataframes
         word_counts = pd.DataFrame()
         stacked_df = pd.DataFrame()
 
+        # Conditional statement to find the k most common words if no list
+        # is passed
         if word_list is None:
             word_list = set()
 
+            # Loop creating a list of the union of the k most common words
+            # from each text.
             for text in self.data["wordcount"]:
                 word_list = word_list.union(
                 set(i[0] for i in self.data["wordcount"][text].most_common(k)))
+            # Converts the set of all most common words into a sorted list
             word_list = list(word_list)
             word_list.sort()
-            for text in self.data["wordcount"]:
-                word_counts["Words"] = word_list
 
-            # for text in self.data["wordcount"].keys():
-                word_counts["Text"] = text
-                word_counts["Frequency"] = list(self.data[
-                                "wordcount"][text][word] for word in word_list)
-                stacked_df = pd.concat([
-                    stacked_df, word_counts], ignore_index=True, sort=False)
-        else:
+            # Loop iterating through all the texts in the class and saving
+            # the text name and word frequencies for all the most common
+            # words to a dataframe.
             for text in self.data["wordcount"]:
                 word_counts["Words"] = word_list
                 word_counts["Text"] = text
                 word_counts["Frequency"] = list(self.data[
                                 "wordcount"][text][word] for word in word_list)
+                # Adds each separate dataframe from each text to a singular
+                # dataframe (stacked format for sankey plotting)
                 stacked_df = pd.concat([
                     stacked_df, word_counts], ignore_index=True, sort=False)
+        # Conditional else statement for if a list of words has been passed
+        else:
+            # Loop iterating through all texts in the class, saving the text
+            # name and word frequencies for all words in the word list param
+            for text in self.data["wordcount"]:
+                word_counts["Words"] = word_list
+                word_counts["Text"] = text
+                word_counts["Frequency"] = list(self.data[
+                                "wordcount"][text][word] for word in word_list)
+                # Concatenates separate dataframes for each text to one df
+                stacked_df = pd.concat([
+                    stacked_df, word_counts], ignore_index=True, sort=False)
+        # Generates and displays sankey
         show_sankey(stacked_df, "Text", "Words", vals = "Frequency")
 
     def sentiment_analysis(self):
-        all_words = ""
+        """Generates sentiment scores for each text using VADER library
+        to get a score for each word and average those scores across each
+        text."""
+
         total_sentiment = []
         text_sentiments = []
+        # Creates the sentiment analyzer object from the library
         analyzer = vs.SentimentIntensityAnalyzer()
 
+        # Loop iterating through all texts in the class
         for text in self.data["wordcount"]:
+            # Iterates through each word in the text
             for i in self.data["wordcount"][text].keys():
+                # Adds the polarity score from the analyzer for that word
+                # multiplied by the number of times the word appears to new list
                 total_sentiment.append(analyzer.polarity_scores(i)[
                                 "compound"] * self.data["wordcount"][text][i])
+            # Averages text sentiment scores for the whole text
             text_sentiments.append(sum(total_sentiment) / self.data[
                                                             "numwords"][text])
-
-    def LDA(self):
-        pass
 
 def main():
 
     T = Textinator()
+    print(STOP_WORDS_FILENAME)
     T.load_stop_words(STOP_WORDS_FILENAME)
 
-    # T.load_text('data/cig_data/independent_1.pdf', 'I1', parser=T.pdf_parser)
-    # T.load_text('data/cig_data/independent_2.pdf', 'I2', parser=T.pdf_parser)
+    T.load_text('data/cig_data/independent_1.pdf', 'I1', parser=T.pdf_parser)
+    T.load_text('data/cig_data/independent_2.pdf', 'I2', parser=T.pdf_parser)
     # T.load_text('data/cig_data/independent_3.pdf', 'I3', parser=T.pdf_parser)
     # T.load_text('data/cig_data/independent_4.pdf', 'I4', parser=T.pdf_parser)
     # T.load_text('data/cig_data/independent_5.pdf', 'I5', parser=T.pdf_parser)
     # T.load_text('data/cig_data/independent_6.pdf', 'I6', parser=T.pdf_parser)
-    # T.load_text('data/cig_data/industry_sponsored_1.pdf', 'S1', parser=T.pdf_parser)
+    T.load_text('data/cig_data/industry_sponsored_1.pdf', 'S1', parser=T.pdf_parser)
     # T.load_text('data/cig_data/industry_sponsored_2.pdf', 'S2', parser=T.pdf_parser)
     # T.load_text('data/cig_data/industry_sponsored_3.pdf', 'S3', parser=T.pdf_parser)
     # T.load_text('data/cig_data/industry_sponsored_4.pdf', 'S4', parser=T.pdf_parser)
     # T.load_text('data/cig_data/industry_sponsored_5.pdf', 'S5', parser=T.pdf_parser)
-    # T.load_text('data/cig_data/industry_sponsored_6.txt', 'S6')
+    # T.load_text('data/cig_data/industry_sponsored_6.pdf', 'S6')
     # T.ASBA_scores('data/GPT_sectioned/industry_sponsored_1.txt')
     # T.ASBA_scores('data/GPT_sectioned/industry_sponsored_2.txt')
     # T.ASBA_scores('data/GPT_sectioned/industry_sponsored_3.txt')
     # T.ASBA_scores('data/GPT_sectioned/industry_sponsored_4.txt')
     # T.ASBA_scores('data/GPT_sectioned/industry_sponsored_5.txt')
-    # T.ASBA_scores('data/GPT_sectioned/industry_sponsored_6.txt')
     # T.ASBA_scores('data/GPT_sectioned/independent_1.txt')
     # T.ASBA_scores('data/GPT_sectioned/independent_2.txt')
     # T.ASBA_scores('data/GPT_sectioned/independent_3.txt')
@@ -254,7 +278,7 @@ def main():
 
     T.wordcount_sankey(k=5)
     T.sentiment_analysis()
-    
+
 
 
     # print(T.data)
