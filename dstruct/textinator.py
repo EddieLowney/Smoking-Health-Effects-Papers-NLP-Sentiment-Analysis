0ae2e221b21e4a75069d173d7f7b5e91a9857876
nbusich <usich.n@northeastern.edu>
2024-11-25 14:10:41 -0500

    Added a few files of data

--- dstruct/textinator.py
+++ dstruct/textinator.py
@@ -5,13 +5,13 @@ anything that we want to visualize in the visionator
 Output:
 """
 
-import random as rnd
+
 from collections import defaultdict, Counter
 from pdfminer.high_level import extract_text
 import json
 import os
 import re
-# from myopenai import MyOpenAPI
+from myopenai import MyOpenAPI
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 import dotenv
 import pandas as pd
@@ -20,7 +20,7 @@ from SANKEY import show_sankey
 
 
 dotenv.load_dotenv()
-# api = MyOpenAPI()
+api = MyOpenAPI()
 
 STOP_WORDS_FILENAME = 'data/stop_words.txt'
 
@@ -31,6 +31,20 @@ class Textinator:
         """
         self.data = defaultdict(dict)
         self.stop_list = list()
+
+    def GPT_key_sections(self, text, filename):
+        base_name = os.path.splitext(os.path.basename(filename))[0]
+        output_name = f'data/GPT_sectioned/{base_name}.txt'
+        prompt = """Find the sections of this text that really contribute to\
+        its meaning. Example: find the abstract, key defining sentences,\
+        discussion, and conclusion of a research paper. Only return exactly\
+        what the article says. TEXT: """
+        prompt += text
+        response_text = api.ask(prompt=prompt)
+        response_text = re.sub(r"\*\*.*?\*\*", '', response_text)
+        response_text = re.sub(r"-", '', response_text)
+        with open(output_name, 'w') as file:
+            file.write(response_text)
 
     def load_text(self, filename, label=None, parser=None):
         """ Register a document with the framework.
@@ -75,12 +89,23 @@ class Textinator:
         """ Parse a standard text file and produce
         extract data results in the form of a dictionary. """
 
-        results = {
-            'wordcount': Counter("To be or not to be".split(" ")),
-            'numwords' : rnd.randrange(10, 50)
-        }
+        base_name = os.path.splitext(os.path.basename(filename))[0]
+        output_name = f'data/converted_files/{base_name}.txt'
 
-        return results
+        with open(filename, 'r') as file:
+            text = file.read()
+        self.GPT_key_sections(text, filename)
+
+        with open(output_name, 'w') as file:
+            file.write(text)
+
+        # Filters words
+        cleaned_words = self.filter_words(text.split(" "))
+        # Gets word counts in a Counter datatype
+        wc = Counter(cleaned_words)
+        num = len(cleaned_words)
+
+        return {'wordcount': wc, 'numwords': num}
 
     def json_parser(self, filename):
         f = open(filename)
@@ -91,19 +116,6 @@ class Textinator:
         num = len(words)
         return {'wordcount': wc, 'numwords': num}
 
-    def GPT_key_sections(self, text, filename):
-        base_name = os.path.splitext(os.path.basename(filename))[0]
-        output_name = f'data/GPT_sectioned/{base_name}.txt'
-        prompt = """Find the sections of this text that really contribute to\
-        its meaning. Example: find the abstract, key defining sentences,\
-        discussion, and conclusion of a research paper. Only return exactly\
-        what the article says. TEXT: """
-        prompt += text
-        response_text = api.ask(prompt=prompt)
-        response_text = re.sub(r"\*\*.*?\*\*", '', response_text)
-        response_text = re.sub(r"-", '', response_text)
-        with open(output_name, 'w') as file:
-            file.write(response_text)
 
     def pdf_parser(self, filename):
         """Called to parse a PDF file. Extracts text. Uses a PDF library
@@ -114,8 +126,8 @@ class Textinator:
         output_name = f'data/converted_files/{base_name}.txt'
 
         text = extract_text(filename)
-        # Writes most important portions of text to separate files using GPT
-        # self.GPT_key_sections(text, filename)
+        #Writes most important portions of text to separate files using GPT
+        self.GPT_key_sections(text, filename)
         with open(output_name, 'w') as file:
             file.write(text)
 
@@ -207,28 +219,32 @@ class Textinator:
             text_sentiments.append(sum(total_sentiment) / self.data[
                                                             "numwords"][text])
 
+    def LDA(self):
+        pass
+
 def main():
 
     T = Textinator()
     T.load_stop_words(STOP_WORDS_FILENAME)
 
-    T.load_text('data/cig_data/independent_1.pdf', 'I1', parser=T.pdf_parser)
-    T.load_text('data/cig_data/independent_2.pdf', 'I2', parser=T.pdf_parser)
+    # T.load_text('data/cig_data/independent_1.pdf', 'I1', parser=T.pdf_parser)
+    # T.load_text('data/cig_data/independent_2.pdf', 'I2', parser=T.pdf_parser)
     # T.load_text('data/cig_data/independent_3.pdf', 'I3', parser=T.pdf_parser)
     # T.load_text('data/cig_data/independent_4.pdf', 'I4', parser=T.pdf_parser)
     # T.load_text('data/cig_data/independent_5.pdf', 'I5', parser=T.pdf_parser)
     # T.load_text('data/cig_data/independent_6.pdf', 'I6', parser=T.pdf_parser)
-    T.load_text('data/cig_data/industry_sponsored_1.pdf', 'S1', parser=T.pdf_parser)
+    # T.load_text('data/cig_data/industry_sponsored_1.pdf', 'S1', parser=T.pdf_parser)
     # T.load_text('data/cig_data/industry_sponsored_2.pdf', 'S2', parser=T.pdf_parser)
     # T.load_text('data/cig_data/industry_sponsored_3.pdf', 'S3', parser=T.pdf_parser)
     # T.load_text('data/cig_data/industry_sponsored_4.pdf', 'S4', parser=T.pdf_parser)
     # T.load_text('data/cig_data/industry_sponsored_5.pdf', 'S5', parser=T.pdf_parser)
-    # T.load_text('data/cig_data/industry_sponsored_6.pdf', 'S6')
+    # T.load_text('data/cig_data/industry_sponsored_6.txt', 'S6')
     # T.ASBA_scores('data/GPT_sectioned/industry_sponsored_1.txt')
     # T.ASBA_scores('data/GPT_sectioned/industry_sponsored_2.txt')
     # T.ASBA_scores('data/GPT_sectioned/industry_sponsored_3.txt')
     # T.ASBA_scores('data/GPT_sectioned/industry_sponsored_4.txt')
     # T.ASBA_scores('data/GPT_sectioned/industry_sponsored_5.txt')
+    # T.ASBA_scores('data/GPT_sectioned/industry_sponsored_6.txt')
     # T.ASBA_scores('data/GPT_sectioned/independent_1.txt')
     # T.ASBA_scores('data/GPT_sectioned/independent_2.txt')
     # T.ASBA_scores('data/GPT_sectioned/independent_3.txt')
@@ -238,7 +254,7 @@ def main():
 
     T.wordcount_sankey(k=5)
     T.sentiment_analysis()
-
+    
 
 
     # print(T.data)
